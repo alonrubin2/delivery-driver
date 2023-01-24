f4fa9bb95cda66cf8fa3d1fe2c9f7ae8434f2819
alonrubin2 <alonrubin2@gmail.com>
2023-01-24 14:54:51 +0200

    added drift

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float turnSpeed = 0.5f;
-    [SerializeField] float moveSpeed = 0.05f;
+    [SerializeField]
+    int turnSpeed = 250;
+
+    [SerializeField]
+    int moveSpeed = 30;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +19,16 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float turnAmount = Input.GetAxis("Horizontal")*turnSpeed;
-        float exelaration = Input.GetAxis("Vertical")*moveSpeed;
-        transform.Rotate(0,0,-turnAmount);
-        transform.Translate(0,exelaration,0);
+        float frameRate = Time.deltaTime;
+        bool drift = Input.GetAxis("handBreak") > 0;
+        float turnAmount = Input.GetAxis("Horizontal") * turnSpeed * frameRate;
+        if (drift)
+        {
+            turnAmount *= 2;
+        }
 
+        float exelaration = Input.GetAxis("Vertical") * moveSpeed * frameRate;
+        transform.Rotate(0, 0, -turnAmount);
+        transform.Translate(0, exelaration, 0);
     }
 }
